75cb0ae787879aa49993c858917574ee67b8c33c
MihaiOsan <mihai.osan01@e-uvt.ro>
2023-04-02 17:35:46 +0300

    frontend login

--- frontend/src/app/sign-up-form/sign-up-form.component.ts
+++ frontend/src/app/sign-up-form/sign-up-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpFormComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  
+  constructor(private formBuilder: FormBuilder, private router: Router, private authenticationService: AuthenticationService) {
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/home']);
+    }
+   }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.registerForm.value);
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService.register(this.f['name'].value, this.f['phone'].value, this.f['email'].value, this.f['password'].value)
+    .pipe(first())
+    .subscribe(
+      (data) => {
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        this.loading = false;
+      }
+    );
+    this.router.navigate(['/LogIn']);
   }
 
 }
